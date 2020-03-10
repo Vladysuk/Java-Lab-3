c63987a3e204302d63b33586c314394a7aea5e53
Vladysuk <57788374+Vladysuk@users.noreply.github.com>
2020-03-10 23:50:20 +0200

    Add files via upload

--- src/main/java/ua/lviv/iot/fishing/model/IceRod.java
+++ src/main/java/ua/lviv/iot/fishing/model/IceRod.java
@@ -2,8 +2,22 @@ package ua.lviv.iot.fishing.model;
 
 public class IceRod extends AbstractFishingRod {
 
-    public IceRod(float lengthInMeters, Season season, float foldedLengthInMeters, int numberOfSection, float weightInKg) {
+    private String typeOfFishingLure;
+
+    public IceRod(float lengthInMeters, Season season, float foldedLengthInMeters, int numberOfSection, float weightInKg, String typeOfFishingLure) {
         super(lengthInMeters, season, foldedLengthInMeters, numberOfSection, weightInKg);
+        this.typeOfFishingLure = typeOfFishingLure;
     }
 
+    public String getTypeOfFishingLure() {
+        return typeOfFishingLure;
+    }
+
+    public String getHeaders() {
+        return super.getHeaders() + ", typeOfFishingLure";
+    }
+
+    public String toCSV() {
+        return super.toCSV() + ", " +  "typeOfFishingLure = " + getTypeOfFishingLure();
+    }
 }
--- src/main/java/ua/lviv/iot/fishing/model/Spinning.java
+++ src/main/java/ua/lviv/iot/fishing/model/Spinning.java
@@ -2,8 +2,22 @@ package ua.lviv.iot.fishing.model;
 
 public class Spinning extends AbstractFishingRod {
 
-    public Spinning(float lengthInMeters, Season season, float foldedLengthInMeters, int numberOfSection, float weightInKg) {
+    private String typeOfWobbler;
+
+    public Spinning(float lengthInMeters, Season season, float foldedLengthInMeters, int numberOfSection, float weightInKg, String typeOfWobbler) {
         super(lengthInMeters, season, foldedLengthInMeters, numberOfSection, weightInKg);
+        this.typeOfWobbler = typeOfWobbler;
     }
 
+    public String getTypeOfWobbler() {
+        return typeOfWobbler;
+    }
+
+    public String getHeaders() {
+        return super.getHeaders() + ", typeOfWobbler";
+    }
+
+    public String toCSV() {
+        return super.toCSV() + ", " + "typeOfWobbler = " + getTypeOfWobbler();
+    }
 }
